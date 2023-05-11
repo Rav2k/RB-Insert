dfaa2345dd06cb6c89842162da5af244669b73b3
Rav2k <wade.owo@gmail.com>
2023-05-11 18:01:43 +0100

    casses work but print is infinte looping on '8' input

--- old.cpp
+++ old.cpp
@@ -10,10 +10,10 @@ using namespace std;
 void fileGen();
 void insert(node *&cur, int number);
 void print(node *cur2, int layers);
-void cases(node*&cur3);
+int cases(node*&cur3);
 void case0(node*&cur3);
-void case2(node*&cur3);
-void case3(node*&cur3);
+int case2(node*&cur3);
+int case3(node*&cur3);
 node *root = NULL;
 
 int main(){//number 1 is black number 2 is red 
@@ -35,6 +35,7 @@ int main(){//number 1 is black number 2 is red
     else{
       cout<<"Invalid!!"<<endl;
       cout<<endl;
+      loop = true;
     }
     
   }while(!loop);
@@ -86,35 +87,72 @@ void insert(node *&cur, int number){//adding in a number to the tree!(all the nu
       cur->parent = NULL;
       return;
     }
+    
     if(number>=cur->data){//if the number is greater than the current node which is the "root" this case then...
       if(cur->right == NULL){//... check to make sure the right is empty then put that number there
 	cur->right = new node(2, number, NULL, NULL, cur);
 	cur->right->parent = cur;
-	cases(cur->right);
 	cout<<endl;
-	return;
+        //case0(cur->left);
+        if(cases(cur->right) == 1){
+          return;
+        }//red
+        else{
+          //do nothing
+        }
+        if(case2(cur->right) == 1){
+          return;
+        }//triangle
+        else{
+          //do nothing
+        }
+        if(case3(cur->right) == 1){
+          return;
+        }///line
+        else{
+          return;
+        }
       }
       else{
 	insert(cur->right, number);//if there is something there already go to the right one which is the higher and run this function again to see where it can fit
-	cur->right->parent = cur;
+	//cur->right->parent = cur;
       }
     }
     else{//same idea as the right one but with when the data that will be inserted is less than that of the current's data
       if(cur->left == NULL){
 	cur->left = new node(2, number, NULL, NULL, cur);
 	cur->left->parent = cur;
-	cases(cur->left);
-	return;
+	//case0(cur->left);
+        if(cases(cur->left) == 1){
+	  return;
+	}//red
+	else{
+	  cout<<"Did not do case 1"<<endl;
+	  //do nothing
+	}
+	if(case2(cur->left) == 1){
+	  cout<<"case 2 finished"<<endl;
+	  return;
+	}//triangle
+	else{
+	  cout<<"Did not do case 2"<<endl;
+	  //do nothing
+	}
+        if(case3(cur->left)==1){
+	  return;
+	}///line
+	else{
+	  cout<<"Did not do case 3"<<endl;
+	  return;
+	}
       }
       else{
 	insert(cur->left, number);
-	cur->left->parent = cur;//set the parent of the left to the cur
+	//cur->left->parent = cur;//set the parent of the left to the cur
       }
     }
     case0(cur);
-    cases(cur);
-    case2(cur);
- }
+  }
 
 void print(node *cur2, int layers){
   if(cur2 == NULL){
@@ -130,12 +168,12 @@ void print(node *cur2, int layers){
   print(cur2->left, layers+1);//print the left side
 }
 
- void cases(node*&cur3){  
+int cases(node*&cur3){  
   //case #1: Uncle is red
   if(cur3->parent != NULL &&cur3->parent->parent != NULL && cur3->parent->parent->right != NULL && cur3->parent->parent->left != NULL){
-          
+          cout<<"Nothing is NULL"<<endl;
         if(cur3->parent->data>cur3->parent->parent->data && cur3->parent->parent->left != NULL){
-          //cout<<"Uncle is on the left"<<endl;
+          cout<<"Uncle is on the left"<<endl;
           if(cur3->parent->parent->left->color == 2){
             cur3->parent->parent->left->color = 1;
             cur3->parent->color = 1;
@@ -146,8 +184,9 @@ void print(node *cur2, int layers){
               cur3->parent->parent->color = 2;
             }
           }
+	  return 1;
         }else if(cur3->parent->data<cur3->parent->parent->data && cur3->parent->parent->right != NULL){
-          //cout<<"Uncle is on right"<<endl;
+          cout<<"Uncle is on right"<<endl;
           if(cur3->parent->parent->right->color == 2){
             cur3->parent->parent->right->color = 1;
 	    cur3->parent->parent->color = 2;
@@ -159,10 +198,11 @@ void print(node *cur2, int layers){
 	      }
           }
         }
-	
+	return 1;
        }
-  return;
+  return 0;
 }
+
 //used the smae generating function as binary tree
 void fileGen(){//taking the numbers from the text file and making the base tree
   ifstream numbers("numbers2.txt");
@@ -180,8 +220,9 @@ void fileGen(){//taking the numbers from the text file and making the base tree
     num = nums.at(randomIndex);
     insert(root, num);
     counter++;
+    
   }
-
+  cout<<"done";
 }
 
 void case0(node*&cur3){
@@ -198,86 +239,49 @@ void case0(node*&cur3){
 
 }
 
-void case2(node*& cur3) {
-  if (cur3->parent != NULL && cur3->parent->parent != NULL) {
-    if (cur3->parent->data > cur3->parent->parent->data && cur3->parent->parent->left != NULL) {
-      // Uncle is on the left
-      if (cur3->parent->parent->left->color == 1 && cur3->parent->left == cur3) {
-        cur3->parent->parent->right = cur3;
-        cur3->right = cur3->parent;
-
-        // Make sure everything points correctly
-        if (cur3->parent->parent != NULL) {
-          cur3->parent->parent->right->parent = cur3->parent->parent;
-          cur3->right->parent = cur3;
-        }
-      }
-    } else if (cur3->parent->data < cur3->parent->parent->data && cur3->parent->parent->right != NULL) {
-      // Uncle is on the right
-      if (cur3->parent->parent->right->color == 1 && cur3->parent->right == cur3) {
-        node* temp = cur3->left;
-        node* temp2 = cur3->parent->parent;
-        cur3->parent->parent->left = cur3;
-        cur3->left = cur3->parent;
-
-        // Make sure everything points correctly
-        cur3->parent->right = temp;
-        if (cur3->parent->parent != NULL) {
-          cur3->parent->parent->left->parent = temp2;
-          cur3->left->parent = cur3;
-        }
-      }
-    }
-  }
-}
-
-
-/*void case2(node*&cur3){
-  if(cur3->parent != NULL &&cur3->parent->parent != NULL && cur3->parent->parent->right != NULL && cur3->parent->parent->left != NULL){
-
-    if(cur3->parent->data>cur3->parent->parent->data && cur3->parent->parent->left != NULL){
+int case2(node*&cur3){
+  cout<<cur3<<endl;
+  if(cur3->parent !=NULL && cur3->parent->parent != NULL){
+    if(cur3->parent->data > cur3->parent->parent->data){
       //Uncle is on the left
-      if(cur3->parent->parent->left->color = 1 && cur3->parent->left == cur3){
-        cur3->parent->parent->right = cur3;
-        cur3->right = cur3->parent;
-
+      if((cur3->parent->parent->left == NULL || cur3->parent->parent->left->color == 1) && cur3->parent->left == cur3 && cur3->parent->color == 2){
+	node *temp2 = cur3->parent;
+	cur3->parent->parent->right = cur3;
+        cur3->right = temp2;
         //making sure everthing points right
         cur3->parent->parent->right->parent = cur3->parent->parent;
         cur3->right->parent = cur3;
-        //return;
+        return 1;
       }else{
         //do nothing
       }
     }
 
-    else if(cur3->parent->data<cur3->parent->parent->data && cur3->parent->parent->right != NULL){
+    else if(cur3->parent->data<cur3->parent->parent->data){
       //Uncle is on right
-      if(cur3->parent->parent->right->color == 1 && cur3->parent->right == cur3){
-        node *temp;
-        node *temp2;
-        temp = cur3->left;
-        temp2 = cur3->parent->parent;
-        cur3->parent->parent->left = cur3;
-        cur3->left = cur3->parent;
-        //making everything right
-        cur3->parent->right = temp;
-        cur3->parent->parent->left->parent = temp2;
+      if((cur3->parent->parent->right == NULL || cur3->parent->parent->right->color == 1) && cur3->parent->right == cur3 && cur3->parent->color == 2){
+	node *temp = cur3->parent;
+	cur3->parent->parent->left = cur3;
+	cur3->left = temp;
+        //making sure everthing points right
+        cur3->parent->parent->left->parent = cur3->parent->parent;
         cur3->left->parent = cur3;
-        //return;
+	return 1;
       }else{
         //do nothing
       }
     }
+    
   }
-  return;
+return 0;
 }
-*/
-/*void case3(node*&cur3){
-  if(cur3->parent != NULL &&cur3->parent->parent != NULL && cur3->parent->parent->right != NULL && cur3->parent->parent->left != NULL){
 
-      if(cur3->parent->data>cur3->parent->parent->data && cur3->parent->parent->left != NULL){
+
+int case3(node*&cur3){
+  if(cur3->parent != NULL && cur3->parent->parent != NULL){
+      if(cur3->parent->data>cur3->parent->parent->data){
         //uncle on left
-        if(cur3->parent->parent->left->color = 1 && cur3->parent->right == cur3){
+        if((cur3->parent->parent->left == NULL || cur3->parent->parent->left->color == 1)&& cur3->parent->right == cur3){
           if(cur3->parent->left!=NULL){
             cur3->parent->parent->right = cur3->parent->left;
           }
@@ -286,16 +290,12 @@ void case2(node*& cur3) {
           cur3->parent->right = cur3;
           cur3->parent->color = 1;
           cur3->parent->parent->color = 2;
-          return;
-        }else{
-          //do nothing
-          return;
-        }
+          return 1;
       }
 
       else if(cur3->parent->data<cur3->parent->parent->data && cur3->parent->parent->right != NULL){
         //uncle is right
-        if(cur3->parent->parent->right->color = 1 && cur3->parent->left == cur3){
+        if(cur3->parent->parent->right->color == 1 && cur3->parent->left == cur3){
           if(cur3->parent->right != NULL){
             cur3->parent->parent->left = cur3->parent->right;
           }
@@ -304,58 +304,24 @@ void case2(node*& cur3) {
           cur3->parent->left = cur3;
           cur3->parent->color = 1;
           cur3->parent->parent->color = 2;
-          return;
-        }else{
-          //do nothing
-          return;
-        }
-      }
-      }
-  
-}
-*/
-void case3(node*& cur3) {
-  if (cur3->parent != NULL && cur3->parent->parent != NULL &&
-      cur3->parent->parent->right != NULL && cur3->parent->parent->left != NULL) {
+          return 1;
 
-    if (cur3->parent->data > cur3->parent->parent->data &&
-        cur3->parent->parent->left != NULL) {
-      // Uncle on the left
-      if (cur3->parent->parent->left->color == 1 && cur3->parent->right == cur3) {
-        if (cur3->parent->left != NULL) {
-          cur3->parent->parent->right = cur3->parent->left;
+	  /*
+	     node *temp;
+228        node *temp2;
+229        temp = cur3->left;
+230        temp2 = cur3->parent->parent;
+231        cur3->parent->parent->left = cur3;
+232        cur3->left = cur3->parent;
+233        //making everything right
+234        cur3->parent->right = temp;
+235        cur3->parent->parent->left->parent = temp2;
+236        cur3->left->parent = cur3;
+	   */
         }
-        cur3->parent->left = cur3->parent->parent;
-        if (cur3->parent->parent->left != NULL) {
-          cur3->parent->parent->left->parent = cur3->parent->parent;
-        }
-        cur3->parent->right = cur3;
-        cur3->parent->color = 1;
-        cur3->parent->parent->color = 2;
-        return;
-      } else {
-        // Do nothing
-        return;
       }
-    } else if (cur3->parent->data < cur3->parent->parent->data &&
-               cur3->parent->parent->right != NULL) {
-      // Uncle is on the right
-      if (cur3->parent->parent->right->color == 1 && cur3->parent->left == cur3) {
-        if (cur3->parent->right != NULL) {
-          cur3->parent->parent->left = cur3->parent->right;
-        }
-        cur3->parent->right = cur3->parent->parent;
-        if (cur3->parent->parent->right != NULL) {
-          cur3->parent->parent->right->parent = cur3->parent->parent;
-        }
-        cur3->parent->left = cur3;
-        cur3->parent->color = 1;
-        cur3->parent->parent->color = 2;
-        return;
-      } else {
-        // Do nothing
-        return;
+      return 0;
       }
-    }
-  }
+}
+  return 0;
 }
